edf4ee1141d099b78902507df5e650fd69f43c72
Paulo Vinicius Pachiani de Mello <paulovmello.99@outlook.com.br>
2019-04-05 21:36:38 -0300

    Progress Bar Fix
    Fixing PB style

--- app/src/main/java/com/studio/pci/activities/BaseActivity.java
+++ app/src/main/java/com/studio/pci/activities/BaseActivity.java
@@ -6,8 +6,6 @@ import android.support.annotation.VisibleForTesting;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.LinearLayout;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.studio.pci.R;
@@ -19,7 +17,7 @@ public class BaseActivity extends AppCompatActivity {
 
     protected void showProgressDialog() {
         if (mProgressDialog == null) {
-            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog = new ProgressDialog(this,R.style.AppCompatProgressBarStyle);
             mProgressDialog.setTitle(getString(R.string.wait));
             mProgressDialog.setMessage(getString(R.string.loading));
             mProgressDialog.setIndeterminate(true);
